d3e8ece5ac4ddf25d35a5dec909fb0899cac3bd4
FzZarezadeh <fatmhzhrazarzadh@gmail.com>
2025-12-09 10:54:37 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int fact(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * fact(n - 1);
+
+        }
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("enter number:");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Factorial of {0} = {1}",n1,fact(n1));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
